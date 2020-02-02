7961b7eec69516266f105167b7073d6a99392da9
ogawa_shiori <ogawa_shiori@applibot.co.jp>
2020-02-03 00:23:20 +0900

    chap2

--- chapter2/kadai.go
+++ chapter2/kadai.go
@@ -1,17 +1,30 @@
 package chapter2
 
+import (
+	"fmt"
+)
+
 // 引数のスライスsliceの要素数が
 // 0の場合、0とエラー
 // 2以下の場合、要素を掛け算
 // 3以上の場合、要素を足し算
 // を返却。正常終了時、errorはnilでよい
 func Calc(slice []int) (int, error) {
-	// TODO Q1
-	// ヒント：エラーにも色々な生成方法があるが、ここではシンプルにfmtパッケージの
-	// fmt.Errorf(“invalid op=%s”, op) などでエラー内容を返却するのがよい
-	// https://golang.org/pkg/fmt/#Errorf
-
-	return 0, nil
+	l := len(slice)
+	switch l {
+	case 0 :
+		return 0, fmt.Errorf("sliceのサイズが0です")
+	case 1 :
+		return slice[0], nil
+	case 2 :
+		return slice[0] * slice[1], nil
+	default:
+		var sum int
+		for i := range slice {
+			sum += slice[i]
+		}
+		return sum, nil
+	}
 }
 
 type Number struct {
@@ -21,17 +34,24 @@ type Number struct {
 // 構造体Numberを3つの要素数から成るスライスにして返却
 // 3つの要素の中身は[{1} {2} {3}]とし、append関数を使用すること
 func Numbers() []Number {
-	// TODO Q2
-
-	return nil
+	nums := make([]Number, 0, 3)
+	nums = append(nums, Number{index:1})
+	nums = append(nums, Number{index:2})
+	nums = append(nums, Number{index:3})
+	return nums
 }
 
 // 引数mをforで回し、「値」部分だけの和を返却
 // キーに「yon」が含まれる場合は、キー「yon」に関連する値は除外すること
 func CalcMap(m map[string]int) int {
-	// TODO Q3
-
-	return 0
+	var sum int
+	for key, val := range m {
+		if key == "yon" {
+			continue
+		}
+		sum += val
+	}
+	return sum
 }
 
 type Model struct {
@@ -40,22 +60,36 @@ type Model struct {
 
 // 与えられたスライスのModel全てのValueに5を足す破壊的な関数を作成
 func Add(models []Model) {
-	// TODO  Q4
-
+	for i := range models {
+		models[i].Value += 5
+	}
 }
 
 // 引数のスライスには重複な値が格納されているのでユニークな値のスライスに加工して返却
 // 順序はスライスに格納されている順番のまま返却すること
 // ex) 引数:[]slice{21,21,4,5} 戻り値:[]int{21,4,5}
 func Unique(slice []int) []int {
-	// TODO Q5
+	uniqueMap := make(map[int]struct{})
+	result := make([]int, 0, len(slice))
 
-	return nil
+	for _, val := range slice {
+		if _, ok := uniqueMap[val]; ok {
+			continue
+		}
+		uniqueMap[val] = struct{}{}
+		result = append(result, val)
+	}
+	return result
 }
 
 // 連続するフィボナッチ数(0, 1, 1, 2, 3, 5, ...)を返す関数(クロージャ)を返却
 func Fibonacci() func() int {
-	// TODO Q6 オプション
-
-	return nil
+	var (
+		ex1 = 0
+		ex2 = 1
+	)
+	return func() int {
+		ex1, ex2 = ex1+ex2, ex1
+		return ex2
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/apbgo/go-study-group
 go 1.13
 
 require (
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
